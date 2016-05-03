99fb47f06ef35e86a7ff677695301ea1e5c86cd3
Roy Halevi <royhalevi@gmail.com>
2016-05-03 15:33:47 +0300

    Update flask_service.py

--- ida/ida-service/flask_service.py
+++ ida/ida-service/flask_service.py
@@ -21,7 +21,7 @@ def execute_command():
 
     command = request.form['command']
     if not command.startswith('idal ') and not command.startswith('idal64 '):
-        return jsonify(error="'command' should start with 'idal' or 'idal64"), 422
+        return jsonify(error="'idal' and 'idal64' are the only valid commands"), 422
 
     try:
         print('Executing %s' % command)
