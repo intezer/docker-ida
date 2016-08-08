5402758e9a6fd809bc5cc7538e230b2edec5ef50
david tufik <davidtufik@gmail.com>
2016-08-08 13:33:55 +0300

    Added timeout option to the server

--- ida/ida-service/ida_service.sh
+++ ida/ida-service/ida_service.sh
@@ -1,3 +1,32 @@
 #!/usr/bin/env bash
-CORES=${1:-8}
-gunicorn -w $CORES -b 0.0.0.0:4000 flask_service:app
+
+while :
+do
+    case "$1" in
+      -c | --core)
+    	  CORES="$2"
+    	  shift 2
+    	  ;;
+      -t | --timeout)
+    	  TIMEOUT="$2"
+    	  shift 2
+    	  ;;
+      --)
+        shift
+        break;
+        ;;
+      -*)
+        echo "Error: Unknown option: $1" >&2
+        exit 1
+        ;;
+      *)  # No more options
+    	  break
+    	  ;;
+    esac
+done
+
+
+if [ -z ${CORES+x} ]; then CORES=8;fi
+if [ -z ${TIMEOUT+x} ]; then TIMEOUT=30;fi
+
+gunicorn -w $CORES -t $TIMEOUT -b 0.0.0.0:4000 flask_service:app
