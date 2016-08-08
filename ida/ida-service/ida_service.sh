55bcbbe6b0cf5fec5a1aaf90cfc187c8e7e588cd
david tufik <davidtufik@gmail.com>
2016-08-08 14:16:53 +0300

    fixe parameters not been validate correctly

--- ida/ida-service/ida_service.sh
+++ ida/ida-service/ida_service.sh
@@ -1,23 +1,32 @@
 #!/usr/bin/env bash
 
+re='^[0-9]+$'
+
 while :
 do
     case "$1" in
       -c | --core)
     	  CORES="$2"
+        if [ -z ${CORES} ]; then
+          echo "Error: cores must be set" >&2; exit 1
+        fi
+        if ! [[ $CORES =~ $re ]] ; then
+          echo "Error: $CORES is not a number" >&2; exit 1
+        fi
     	  shift 2
     	  ;;
       -t | --timeout)
     	  TIMEOUT="$2"
+        if [ -z ${TIMEOUT} ]; then
+          echo "Error: timeout must be set" >&2; exit 1
+        fi
+        if ! [[ $TIMEOUT =~ $re ]] ; then
+          echo "Error: $TIMEOUT is not a number" >&2; exit 1
+        fi
     	  shift 2
     	  ;;
-      --)
-        shift
-        break;
-        ;;
-      -*)
-        echo "Error: Unknown option: $1" >&2
-        exit 1
+      ?*)
+        echo "Error: Unknown option: $1" >&2; exit 1
         ;;
       *)  # No more options
     	  break
